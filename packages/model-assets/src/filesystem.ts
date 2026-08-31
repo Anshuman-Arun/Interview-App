4c61a3959e651128fc3ad0e5d67ed0ef5c0c2f9e
Anshuman-Arun <anshumanarun2008@gmail.com>
2026-08-30 23:30:07 -0700

    Name manager-owned removal tombstones explicitly

--- packages/model-assets/src/filesystem.ts
+++ packages/model-assets/src/filesystem.ts
@@ -22,6 +22,8 @@ import {
 } from "./types.js";
 
 export const MAX_STORED_MANIFEST_BYTES = 64 * 1024;
+export const REMOVAL_TOMBSTONE_PATTERN =
+  /^\.model-assets-delete-[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/u;
 
 export interface FileVerificationExpectations {
   readonly sizeBytes: number;
