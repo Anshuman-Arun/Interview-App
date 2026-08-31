559883b3eb4e1fd4a82f7ec9ae2f4d66b23c8562
Anshuman-Arun <anshumanarun2008@gmail.com>
2026-08-31 07:35:22 -0700

    Keep internal environment snapshot helper out of public exports

--- packages/local-runtime/src/index.ts
+++ packages/local-runtime/src/index.ts
@@ -1,3 +1,8 @@
-export * from "./environment.js";
+export {
+  DEFAULT_POSIX_INHERITED_ENVIRONMENT_KEYS,
+  DEFAULT_WINDOWS_INHERITED_ENVIRONMENT_KEYS,
+  buildLocalEnvironment,
+  type BuiltLocalEnvironment
+} from "./environment.js";
 export * from "./manager.js";
 export * from "./types.js";
