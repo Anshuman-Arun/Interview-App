c6c29e117bcdaa389d84387e0849a44a248c5591
Anshuman-Arun <anshumanarun2008@gmail.com>
2026-09-01 06:50:19 -0700

    Isolate Quant Trader exports from concurrent local-compute additions

--- packages/local-compute/src/index.ts
+++ packages/local-compute/src/index.ts
@@ -2,6 +2,6 @@ export * from "./protocol.js";
 export * from "./worker-client.js";
 export * from "./order-book.js";
 export * from "./portfolio-tracker.js";
-export * from "./market-maker.js";
 export * from "./seeded-random.js";
 export * from "./quant-trader-interview.js";
+export * from "./market-maker.js";
