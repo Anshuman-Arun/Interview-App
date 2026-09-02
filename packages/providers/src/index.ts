da138073b0f8165bd54ee96d2e014d737bf8fb63
Anshuman-Arun <anshumanarun2008@gmail.com>
2026-09-01 22:26:39 -0700

    Export supervised CLI provider adapters

--- packages/providers/src/index.ts
+++ packages/providers/src/index.ts
@@ -4,3 +4,6 @@ export * from "./execution.js";
 export * from "./gemini-api-adapter.js";
 export * from "./mock-model-adapter.js";
 export * from "./policy.js";
+
+export * from "./antigravity-cli-adapter.js";
+export * from "./supervised-cli-provider.js";
